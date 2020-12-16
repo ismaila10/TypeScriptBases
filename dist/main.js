ffc77d393b37076bc901a459209da8de78a8deeb
Ismaila Diallo <lodia.ismaila@gmail.com>
2020-12-16 20:22:48 +0100

    Transform ts to js with gulp

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,4 @@
 function helloGulp(arg1) {
     console.log('Gulp : ' + arg1);
 }
+helloGulp('Yo. Ishlolo');
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 function helloGulp(arg1: string) : void {
     console.log('Gulp : ' + arg1);
 }
+
+helloGulp('Yo. Ishlolo');
