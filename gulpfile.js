e415da762632986f553244a6bd89ce2fd32758e9
Ismaila Diallo <lodia.ismaila@gmail.com>
2020-12-17 20:25:28 +0100

    add browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,33 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 
-gulp.task('default', function () {
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var tsify = require("tsify");
+
+gulp.task("html", function () {
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task('default', ["html"], function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest("dist"));
+});
+
+/*gulp.task('default', function () {
     return gulp.src('src/*.ts')
         .pipe(ts({
             noImplicitAny: true
         }))
         .pipe(gulp.dest('dist'));
-});
+});*/
